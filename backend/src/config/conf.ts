66f2313e725f41ec836deacedcf385cc0f46a66b
Mukesh-Kalva <mukeshkalva150@gmail.com>
2025-09-14 21:28:45 +0530

    Fix TS errors for env variables

--- backend/src/config/conf.ts
+++ backend/src/config/conf.ts
@@ -1,10 +1,10 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3001,
+  port: parseInt(process.env.PORT ?? '3001', 10), // default to 3001 if undefined
   database: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST ?? 'localhost',
+    port: parseInt(process.env.DB_PORT ?? '5432', 10), // default to 5432 if undefined
+    username: process.env.DB_USERNAME ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    database: process.env.DB_NAME ?? 'dashboard_db',
   },
 });
